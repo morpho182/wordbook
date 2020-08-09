c65168515ea942894561fd6d960fa889d4242328
morpho182 <morpho182.b@gmail.com>
2020-08-09 23:07:09 +0000

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = 'user は更新されました'
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:danger] = 'user は更新されませんでした'
+      render :edit
+    end
   end
   
   private
